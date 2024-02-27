bf8028e3e74686f7a011593c288cb8d377f9ab4e
Natalia Soares <nataliasilvamvn@gmail.com>
2024-02-27 14:28:14 -0300

    Atualizando pasta Docs, para receber modificações do JSON e aparecer no Deploy

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-RYPCWGGY.mjs";import"./chunk-PV7YN6GH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-Y36J6ZHV.mjs";import"./chunk-PV7YN6GH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
